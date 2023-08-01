dc20df9ec1468efaf4742eb2d1be911fe5232763
abdulwariskhan <hellokhan.ch@gmail.com>
2023-08-01 11:37:25 +0200

    Initial Commit

---
+++ Bulleyes/BulleyesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BulleyesApp.swift
+//  Bulleyes
+//
+//  Created by Khan on 01.08.23.
+//
+
+import SwiftUI
+
+@main
+struct BulleyesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
